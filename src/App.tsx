d2e34e4449b30d512ec90cbf1a3680f7b9433123
Ghenet Andemiachael <ghenikibrom@gmail.com>
2021-03-26 17:30:06 -0700

    add the jsx sketch of app and its functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import QuestionCard from './components/QuestionCard';
 
 
-function App() {
+const App = () => {
+
+  //calls the api
+  const startTrivia = async () => {
+
+  }
+// checks the answers
+const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+}
+//next question button
+const nextQuestion = () => {
+
+}
+
+
   return (
     <div className="App">
-     Quiz
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick= {startTrivia}>
+        start
+      </button>
+      <p className="score">Score: </p>
+      <p>Loading Questions ...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
 }
